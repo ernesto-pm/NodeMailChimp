64e8515d9b3961499b465fd2fa67c1fd39264e18
ErnestoPM <ernestopm20@gmail.com>
2017-03-15 09:39:12 -0600

    Anidianedo ruta hooks

--- app.js
+++ app.js
@@ -10,6 +10,11 @@ const port          = process.env.PORT || 9090;
 const apiKey        = process.env.APIKEY;
 // Termina configuracion de variables del entorno
 
+app.get('/hook',function(req,res){
+    console.log('req',req);
+    console.log('res',res);
+});
+
 app.use(bodyParser.json());
 app.listen(function callback(){
    console.log("Aplicacion corriendo en puerto: "+port);
