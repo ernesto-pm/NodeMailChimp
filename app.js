502fe5f51278cd960312bfe06646727000563b9b
ErnestoPM <ernestopm20@gmail.com>
2017-03-15 09:36:54 -0600

    Aniadiendo configuracion inicial

---
+++ app.js
@@ -0,0 +1,16 @@
+const express       = require('express');
+const app           = express();
+const bodyParser    = require('body-parser');
+const dotenv        = require('dotenv');
+const MailChimpAPI  = require('mailchimp').MailChimpAPI;
+
+// Configuracion de variables del entorno
+dotenv.config();
+const port          = process.env.PORT || 9090;
+const apiKey        = process.env.APIKEY;
+// Termina configuracion de variables del entorno
+
+app.use(bodyParser.json());
+app.listen(function callback(){
+   console.log("Aplicacion corriendo en puerto: "+port);
+});
